7b4625a85e93b20e51d2e730e64b2aeb3f1ffb96
pieterVL <pieter.van.leuven01@gmail.com>
2017-04-07 17:02:41 +0200

    Init project

---
+++ scrab.ts
@@ -0,0 +1,15 @@
+namespace scrab{
+	abstract class SrcabObj{
+		constructor(){};
+	}
+	class Stage extends SrcabObj{
+		constructor() {super();}
+	}
+	class Sprite extends SrcabObj{
+		constructor() {super();}
+	}
+	class CmdList{
+		constructor(){};
+	}
+	export function addSprite():void{}
+}
