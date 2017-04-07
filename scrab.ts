953028182fee5a009053036217b6fdb077f7f5c2
pieterVL <pieter.van.leuven01@gmail.com>
2017-04-07 18:50:49 +0200

    function addSprite body + ScrabObj class

--- scrab.ts
+++ scrab.ts
@@ -1,6 +1,17 @@
 namespace scrab{
 	abstract class SrcabObj{
-		constructor(){};
+		greenFlag:CmdList[];
+		keyPressed:ICmdListGroup;
+		clicked:CmdList[];
+		sceneStarts:ICmdListGroup;
+		sensorGreaterThan:ICmdListGroup;
+		iReceive:ICmdListGroup;
+		constructor(){
+			this.keyPressed={};
+			this.sceneStarts={};
+			this.sensorGreaterThan={};
+			this.iReceive={};
+		};
 	}
 	class Stage extends SrcabObj{
 		constructor() {super();}
@@ -8,8 +19,20 @@ namespace scrab{
 	class Sprite extends SrcabObj{
 		constructor() {super();}
 	}
-	class CmdList{
+	interface ICmdListGroup{[index: string]:CmdList[];}
+	abstract class CmdList{
 		constructor(){};
 	}
-	export function addSprite():void{}
+	class SpriteCmdList extends CmdList{
+		constructor() {super();}
+	}
+	class StageCmdList extends CmdList{
+		constructor() {super();}
+	}
+	export function addSprite(sprite: string):void{
+		if(this.hasOwnProperty(sprite))
+			console.log("The name: "+sprite+" is already in use, please choise an other name");
+		else
+			this[sprite] = new Sprite();
+	}
 }
