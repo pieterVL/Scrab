ed2c6cddee2ef0dd5536d63a558c1ca25c4620b5
pieterVL <pieter.van.leuven01@gmail.com>
2017-04-11 00:50:16 +0200

    Filled out add Event Handlers Methods

--- scrab.ts
+++ scrab.ts
@@ -1,5 +1,5 @@
 namespace scrab{
-	enum ScrabEvents{GreenFlag,KeyPressed,Clicked,SceneStarts,SensorGreaterThan,IReceive}
+	enum ScrabEvents{GreenFlag,KeyPressed,Clicked,SceneStarts,SensorGt,IReceive}
 	abstract class SrcabObj{		
 		protected greenFlag:CmdList[];
 		protected keyPressed:ICmdListGroup;
@@ -22,27 +22,40 @@ namespace scrab{
 				case ScrabEvents.KeyPressed:	this.keyPressed[key].push(cmdList);	break;
 				case ScrabEvents.Clicked:		this.clicked.push(cmdList);			break;
 				case ScrabEvents.SceneStarts:	this.sceneStarts[key].push(cmdList);break;
-				case ScrabEvents.SensorGreaterThan: this.sensorGreaterThan[key].push(cmdList);
+				case ScrabEvents.SensorGt: 		this.sensorGreaterThan[key].push(cmdList);
 					cmdList.sensorvalue=value;
 					break;
-				case ScrabEvents.GreenFlag:		this.greenFlag.push(cmdList);		break;		
+				case ScrabEvents.IReceive:		this.iReceive.push[key](cmdList);	break;		
 				default:break;
 			}
 			return cmdList;
 		}
 	}
 	class Stage extends SrcabObj{
-		GreenFlag():StageCmdList{return super.addCmdList(ScrabEvents.GreenFlag,new StageCmdList());};
+		GreenFlag():			StageCmdList{return super.addCmdList(ScrabEvents.GreenFlag,new StageCmdList());};
+		KeyPressed(key:string):	StageCmdList{return super.addCmdList(ScrabEvents.KeyPressed,new StageCmdList(),key);};
+		Clicked():				StageCmdList{return super.addCmdList(ScrabEvents.Clicked,new StageCmdList());};
+		SceneStarts(key:string):StageCmdList{return super.addCmdList(ScrabEvents.SceneStarts,new StageCmdList(),key);};
+		IReceive(key:string):	StageCmdList{return super.addCmdList(ScrabEvents.IReceive,new StageCmdList(),key);};
+		SensorGreaterThan(key:string,value:number):StageCmdList
+											{return super.addCmdList(ScrabEvents.SensorGt,new StageCmdList(),key,value);};		
 		constructor() {super();}
 
 	}
 	class Sprite extends SrcabObj{
-		GreenFlag(): SpriteCmdList{return super.addCmdList(ScrabEvents.GreenFlag, new SpriteCmdList());}
+		GreenFlag():			SpriteCmdList{return super.addCmdList(ScrabEvents.GreenFlag, new SpriteCmdList());}
+		KeyPressed(key:string):	SpriteCmdList{return super.addCmdList(ScrabEvents.KeyPressed,new SpriteCmdList(),key);};
+		Clicked():				SpriteCmdList{return super.addCmdList(ScrabEvents.Clicked,new SpriteCmdList());};
+		SceneStarts(key:string):SpriteCmdList{return super.addCmdList(ScrabEvents.SceneStarts,new SpriteCmdList(),key);};
+		IReceive(key:string):	SpriteCmdList{return super.addCmdList(ScrabEvents.IReceive,new SpriteCmdList(),key);};
+		SensorGreaterThan(key:string,value:number):SpriteCmdList
+											 {return super.addCmdList(ScrabEvents.SensorGt,new SpriteCmdList(),key,value);};
+		
 		constructor() {super();}
 	}
 	interface ICmdListGroup{[index: string]:CmdList[];}
 	abstract class CmdList{
-		sensorvalue:number;//only for SensorGreaterThan Events
+		private sensorvalue:number;//only for SensorGreaterThan Events
 		constructor(){};
 	}
 	class SpriteCmdList extends CmdList{
